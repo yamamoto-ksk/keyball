121fe84896bf886caea77c03a6eceb91efc42c07
yamamoto-ksk <yamamoto877126@gmail.com>
2025-09-07 12:59:03 +0900

    add cpi settings

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_copy/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_copy/config.h
@@ -37,3 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
+#define KEYBALL_CPI_DEFAULT 1100 // マウス速度 (default: 500)
+#define KEYBALL_SCROLL_DIV_DEFAULT 6 // スクロール速度 (default: 4)
